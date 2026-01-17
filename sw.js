23b691f118c2d2927adc8adb24bde8ea07728a3e
salhiahmed097-cloud <salhi.ahmed097@gmail.com>
2026-01-17 18:11:54 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "pruefung-karten-v1";
+const CACHE_NAME = "pruefung-karten-v2";
 const ASSETS = [
   "./",
   "./index.html",
@@ -24,3 +24,4 @@ self.addEventListener("fetch", (event) => {
     caches.match(event.request).then((cached) => cached || fetch(event.request))
   );
 });
+
